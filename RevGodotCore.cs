123afcdeb27d34f1089c90fa0dd68b2f50a54965
RevyaS <revya25@gmail.com>
2025-11-30 13:52:59 +0800

    Added Pivot Center button for 2D control editor

--- RevGodotCore.cs
+++ RevGodotCore.cs
@@ -5,6 +5,8 @@ using System;
 [Tool]
 public partial class RevGodotCore : EditorPlugin
 {
+	Button pivotButton;
+
 	public override void _EnterTree()
 	{
 		var nodeContainerScript = GD.Load<Script>("res:///addons/rev_godot_core/Components/NodeContainer.cs");
@@ -18,11 +20,52 @@ public partial class RevGodotCore : EditorPlugin
 		var hboxNodeContainerScript = GD.Load<Script>("res:///addons/rev_godot_core/Components/HBoxNodeContainer.cs");
 		var hboxNodeContainerIcon = GD.Load<Texture2D>("res://addons/rev_godot_core/Assets/Icons/HBoxContainer.svg");
 		AddCustomType(nameof(HBoxNodeContainer), nameof(HBoxContainer), hboxNodeContainerScript, hboxNodeContainerIcon);
+
+		pivotButton = new Button
+        {
+            Text = "Center Pivot",
+            Visible = false, // Hidden until a Control is selected
+            TooltipText = "Center Pivot Offset for this Control"
+        };
+
+        pivotButton.Pressed += OnPivotButtonPressed;
+		AddControlToContainer(CustomControlContainer.CanvasEditorMenu, pivotButton);
+
+		EditorInterface.Singleton.GetSelection().SelectionChanged += OnSelectionChanged;
 	}
 
-	public override void _ExitTree()
+    private void OnSelectionChanged()
+    {
+        var selected = EditorInterface.Singleton.GetSelection().GetSelectedNodes();
+        if (selected.Count == 0)
+        {
+            pivotButton.Visible = false;
+            return;
+        }
+        Node node = selected[0];
+        // Only show button for Control nodes
+        pivotButton.Visible = node is Control;
+    }
+
+    private void OnPivotButtonPressed()
+    {
+        var selected = EditorInterface.Singleton.GetSelection().GetSelectedNodes();
+        if (selected.Count == 0) return;
+
+        foreach (Node node in selected)
+        {
+            if (node is Control control)
+            {
+                control.PivotOffset = control.Size / 2;
+            }
+        }
+    }
+
+    public override void _ExitTree()
 	{
 		// Clean-up of the plugin goes here.
+		RemoveControlFromContainer(CustomControlContainer.CanvasEditorMenu, pivotButton);
+        pivotButton.QueueFree();
 	}
 }
 #endif
