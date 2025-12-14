52d8500afbde49ab6d7d29c960c5b57928d61420
RevyaS <revya25@gmail.com>
2025-12-14 20:16:43 +0800

    Added FontSize and exported values for LabelValueContaienr

--- Scenes/LabelValueContainer.cs
+++ Scenes/LabelValueContainer.cs
@@ -1,9 +1,44 @@
 using Godot;
 
+[Tool]
 public partial class LabelValueContainer : HBoxContainer
 {
-	string labelText, valueText;
-	Label label, labelValue;
+	string labelText = null!, valueText = null!;
+	Label label = null!, labelValue = null!;
+	int fontSize = 16;
+
+	[Export]
+	public int FontSize
+    {
+        get => fontSize;
+		set
+        {
+            fontSize = value;
+			Render();
+        }
+    }
+
+	[Export]
+	public string Label
+    {
+        get => labelText;
+		set
+        {
+            labelText = value;
+			Render();
+        }
+    }
+
+	[Export]
+	public string Value
+    {
+        get => valueText;
+		set
+        {
+            valueText = value;
+			Render();
+        }
+    }
 	
 	public override void _Ready()
 	{
@@ -15,6 +50,13 @@ public partial class LabelValueContainer : HBoxContainer
 
 	public void Render() {
 		if(!IsInsideTree()) return;
+
+		label.RemoveThemeFontSizeOverride("font_size");
+		label.AddThemeFontSizeOverride("font_size", FontSize);
+
+		labelValue.RemoveThemeFontSizeOverride("font_size");
+		labelValue.AddThemeFontSizeOverride("font_size", FontSize);
+
 		labelValue.Text = valueText;
 		label.Text = labelText;
 	}
